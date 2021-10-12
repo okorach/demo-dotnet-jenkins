acfa6dfef4abdcb3ab79df0ccf4176e63636fa94
Olivier Korach <olivier.korach@gmail.com>
2021-10-12 18:51:03 +0200

    FIx build

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# Load common environment
-
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install org.jacoco:jacoco-maven-plugin:report \
-   -Dmaven.test.failure.ignore=true \
-   sonar:sonar \
-   -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_TOKEN \
-   -Dsonar.exclusions=pom.xml \
-   $*
-
-exit $?
+cd comp-dotnet; ./build.sh $*; cd -
